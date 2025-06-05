b394b97debf7ea8039ccbd294fd369adbb383983
Timofey898 <113227322+Timofey898@users.noreply.github.com>
2025-06-05 20:18:27 +0700

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -1,14 +1,16 @@
 import re
+import time
 import requests
 import asyncio
 import os
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.client.default import DefaultBotProperties
 
-API_TOKEN =("7554455567:AAGt4JS2-B19iZdKNyFsic7inIK9XjyOXLI")
-CHAT_ID = 966229619
+API_TOKEN = os.getenv("API_TOKEN")
+CHAT_ID = int(os.getenv("CHAT_ID"))
 
 ITEMS_TO_CHECK = [
     "Charm%20%7C%20Die-cast%20AK?filter=",
@@ -18,12 +20,10 @@ ITEMS_TO_CHECK = [
 ]
 
 def parse(name):
-    response = requests.get(f"https://steamcommunity.com/market/listings/730/{name}")
-    name_id_match = re.search(r"Market_LoadOrderSpread\( (?P<item_id>\d+) \)", response.text)
-    if not name_id_match:
-        raise ValueError("Не удалось получить item_nameid")
+    name_id_response = requests.get(f"https://steamcommunity.com/market/listings/730/{name}")
+    name_id = re.search(r"Market_LoadOrderSpread\( (?P<item_id>\d+) \)", name_id_response.text).group("item_id")
 
-    name_id = name_id_match.group("item_id")
+    time.sleep(1)
 
     response = requests.get(
         f"https://steamcommunity.com/market/itemordershistogram?country=RU&language=russian&currency=5&item_nameid={name_id}&two_factor=0"
@@ -32,7 +32,10 @@ def parse(name):
     highest_buy_order = int(response["highest_buy_order"]) / 100
     lowest_sell_order = int(response["lowest_sell_order"]) / 100
 
-    return highest_buy_order, lowest_sell_order, response["buy_order_graph"], response["sell_order_graph"]
+    buy_order_graph = response["buy_order_graph"]
+    sell_order_graph = response["sell_order_graph"]
+
+    return highest_buy_order, lowest_sell_order, buy_order_graph, sell_order_graph
 
 def get_template_ids(name):
     url = f"https://steamcommunity.com/market/listings/730/{name}"
@@ -64,30 +67,30 @@ async def send_steam_data(bot, name):
         filtered_info = match_ids_to_prices(template_ids, sell_graph)
 
         if not filtered_info:
-            print(f"{name} — шаблонов > 66000 не найдено")
+            print('Пусто')
             return
 
         message = (
             f"https://steamcommunity.com/market/listings/730/{name}\n"
             f"Самая высокая цена покупки: {buy_price:.2f} руб.\n"
             f"Самая низкая цена продажи: {sell_price:.2f} руб.\n\n"
-            f"Charm Template ID и цены (ID > 66000):\n"
+            f"Charm Template ID и цены (ID > 70000):\n"
             f"{filtered_info}"
         )
 
         await bot.send_message(chat_id=CHAT_ID, text=message, parse_mode=ParseMode.HTML)
-
     except Exception as e:
-        print(f"Ошибка при обработке {name}: {e}")
-
-async def check_all_items_once(bot):
-    for item in ITEMS_TO_CHECK:
-        await send_steam_data(bot, item)
+        print(f"[Ошибка] {e}")
 
 async def main():
     session = AiohttpSession()
     bot = Bot(token=API_TOKEN, session=session, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await check_all_items_once(bot)
+
+    for item in ITEMS_TO_CHECK:
+        await send_steam_data(bot, item)
+        await asyncio.sleep(2)
+
+    await bot.session.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
